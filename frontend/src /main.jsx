31736fa0cdedd074431c1b677846e6278c538141
natec44 <christophersen44@gmail.com>
2025-06-03 13:43:35 -0700

    Update main.jsx

--- frontend/src /main.jsx
+++ frontend/src /main.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import store from './redux/store';
+import { registerServiceWorker } from './registerServiceWorker';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
+
+registerServiceWorker();
