f5a06a4d3a62fc8c5b7d8b1e02809fda12b504a1
natec44 <christophersen44@gmail.com>
2025-06-02 17:28:12 -0700

    Create usercontroller.js

---
+++ backend/controllers/usercontroller.js
@@ -0,0 +1,19 @@
+import User from '../models/user.js';
+
+export const getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const updateUserRole = async (req, res, next) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, { role: req.body.role }, { new: true });
+    res.json(user);
+  } catch (err) {
+    next(err);
+  }
+};
