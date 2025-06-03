f0bc658c064378f7a2f176d9646d606fc538d5e9
natec44 <christophersen44@gmail.com>
2025-06-03 07:55:29 -0700

    Create notificationslice.js

---
+++ frontend/src /redux/slices/notificationslice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const notificationSlice = createSlice({
+  name: 'notification',
+  initialState: {
+    notifications: [],
+  },
+  reducers: {
+    setNotifications: (state, action) => {
+      state.notifications = action.payload;
+    },
+    addNotification: (state, action) => {
+      state.notifications.push(action.payload);
+    },
+  },
+});
+
+export const { setNotifications, addNotification } = notificationSlice.actions;
+export default notificationSlice.reducer;
