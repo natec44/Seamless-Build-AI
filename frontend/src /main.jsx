4f9cba4fcbec9bbaa93dd17cb76a892d23926bf7
natec44 <christophersen44@gmail.com>
2025-06-04 10:49:49 -0700

    Update main.jsx

--- frontend/src /main.jsx
+++ frontend/src /main.jsx
@@ -1,13 +1,12 @@
+// frontend/src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { registerServiceWorker } from './registerServiceWorker';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = document.getElementById('root');
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-registerServiceWorker();
